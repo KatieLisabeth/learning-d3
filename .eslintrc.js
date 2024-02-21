5025704347fe4be1af6ba2020af01985925427af
Kateryna Lisabeth <kateryna.lisabeth@futureproofed.com>
2024-02-21 22:42:34 +0100

    add better configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
+     "plugin:vue/essential",
+     "plugin:prettier/recommended",
+     "@vue/typescript",
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
@@ -14,8 +15,38 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript'],
   rules: {
-    // Add or modify rules as needed
-  },
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4,
+            },
+        ],
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+        "no-duplicate-imports": "error",
+        // keep minimal set passing
+        "vue/require-v-for-key": 0,
+        "no-dupe-keys": "error",
+
+        // don't allow code after a return statement
+        "no-unreachable": "error",
+
+        // don't allow references to variables or functions that don't exist
+        "no-empty-function": ["error", { allow: ["arrowFunctions"] }],
+        "no-cond-assign": "error",
+        "no-func-assign": "error",
+        "no-inner-declarations": "error",
+        "no-sparse-arrays": "error",
+        "no-template-curly-in-string": "error",
+        "array-callback-return": "error",
+        "no-global-assign": "error",
+        "no-redeclare": "error",
+        "require-await": "error",
+        "import/no-unresolved": 0,
+        "id-blacklist": ["error", "array"],
+        "vue/return-in-computed-property": 0, // always return in computed => disable for now
+        "vue/no-v-html": "warn",
+    },
+
 };
