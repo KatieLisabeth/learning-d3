943073a6697c7388c4b0e95e657a1aaa90d89dc6
Kateryna Lisabeth <kateryna.lisabeth@futureproofed.com>
2024-02-21 21:51:31 +0100

    add configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,16 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard',
+    'eslint:recommended',
     '@vue/typescript/recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     // Add or modify rules as needed
   },
