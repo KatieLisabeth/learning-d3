651288e239c4278376aa59e4cc2fdd05b18e889c
Kateryna Lisabeth <kateryna.lisabeth@futureproofed.com>
2024-02-23 10:43:04 +0100

    rename css files

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-    publicPath: "/",
+    publicPath: "https://katielisabeth.github.io/learning-d3/",
     outputDir: "docs",
     assetsDir: "",
     transpileDependencies: [],
